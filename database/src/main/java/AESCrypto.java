5253de0cfca63f7b4f08a2781362c1c8ae16bd8d
PayamDivsalar <payamdivsalar@aut.ac.ir>
2024-06-27 19:49:03 +0330

    added crypto service code using AES.

---
+++ database/src/main/java/AESCrypto.java
@@ -0,0 +1,48 @@
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class AESCrypto implements CryptoService {
+
+    private SecretKey secretKey;
+
+    public AESCrypto() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(128);
+            this.secretKey = keyGen.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String encrypt(String info) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(info.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+                 | IllegalBlockSizeException | BadPaddingException e) {
+            throw new RuntimeException("Encryption failed", e);
+        }
+    }
+
+    @Override
+    public String decrypt(String encryptedString) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedString));
+            return new String(decryptedBytes);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+                 | IllegalBlockSizeException | BadPaddingException e) {
+            throw new RuntimeException("Decryption failed", e);
+        }
+    }
+
+
+}
