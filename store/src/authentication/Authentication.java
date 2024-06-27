6568949ee134118ec07fa9743d986fcf0e8fc01f
Sami-gh05 <sam.ghorbani8490@gmail.com>
2024-06-28 03:25:14 +0330

    Authentication class created to handle sign in and sign up process

---
+++ store/src/authentication/Authentication.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Authentication {
+    public static class signUpOrSignIn{
+        private static Set<User> users = new HashSet<>();
+        private static Admin admin = new Admin();
+
+        public static void userRegister(String userName, String passWord){
+            //TODO
+        }
+        public static void userLogIn(String userName, String passWord){
+            //TODO
+        }
+
+        public static void AdminLogIN(String userName, String passWord){
+            //TODO
+        }
+    }
+
+}
