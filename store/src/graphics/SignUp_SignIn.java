270e67f3331a2fb0ce244f352926c8de90c28951
Payam Divsalar <payamdivsalar@aut.ac.ir>
2024-06-28 21:54:02 +0330

    Update SignUp_SignIn.java

--- store/src/graphics/SignUp_SignIn.java
+++ store/src/graphics/SignUp_SignIn.java
@@ -1,5 +1,28 @@
 package graphics;
-
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SignUp_SignIn {
+    private MainGUI mainGUI;
+    private JPanel panel;
+
+    //TODO buttons and textfields and sub panels.
+
+    public SignUp_SignIn(MainGUI mainGUI) {
+        this.mainGUI = mainGUI;
+        panel = new JPanel();
+
+        //TODO
+
+    }
+
+    /* suitable actionListener using authentication class,
+       and then, according to user or admin, use mainGUI.showUserMainGUI or mainGUI.showAdminMainGUI
+    */
+
+
+    public JPanel getPanel() {
+        return panel;
+    }
 }
