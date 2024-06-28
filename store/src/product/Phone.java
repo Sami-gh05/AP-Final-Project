31dda1362acf665118a8fdd6084c1c3cb97252be
Sami-gh05 <sam.ghorbani8490@gmail.com>
2024-06-28 04:38:58 +0330

    Class Phone created

---
+++ store/src/product/Phone.java
@@ -0,0 +1,39 @@
+public class Phone extends Product{
+    private String companyName;
+    private String model;
+    private String color;
+    private int adder = 1;
+    public Phone(String title, float price, String companyName, String model, String color){
+        super(title, price);
+        this.companyName = companyName;
+        this.color = color;
+        this.model = model;
+        super.productCode += "PH" + String.valueOf(adder); //because it is Phone
+        adder++;
+        super.label = "Phone";
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
