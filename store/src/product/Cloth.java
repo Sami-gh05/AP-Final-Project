f2a831372572521b232d2f5c0991b25e526d34b4
Sami-gh05 <sam.ghorbani8490@gmail.com>
2024-06-28 04:00:56 +0330

    Cloth sub class created

---
+++ store/src/product/Cloth.java
@@ -0,0 +1,38 @@
+public class Cloth extends Product{
+    private String size;
+    private String color;
+    private String sex;
+    private int adder = 1;
+    public Cloth(String title, float price, String size, String color, String sex){
+        super(title, price);
+        this.size = size;
+        this.color = color;
+        this.sex = sex;
+        super.productCode += "C" + String.valueOf(adder); //because it is cloth
+        adder++;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
