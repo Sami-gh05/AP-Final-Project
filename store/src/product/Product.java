9f72fedd4dce627fc2671afd29f8589e7014d035
Sami-gh05 <sam.ghorbani8490@gmail.com>
2024-06-28 03:59:55 +0330

    getter and setter methods get implemented + productCode and raterNum fields added

--- store/src/product/Product.java
+++ store/src/product/Product.java
@@ -1,17 +1,32 @@
 public abstract class Product {
+    protected String productCode;
     private String title;
     private float price;
-    private short rate;
+    private int rate, ratersNum;
+
     public Product(String title, float price){
         this.title = title;
         this.price = price;
-        this.rate = 0;
+        this.rate = 1;
+        this.productCode = "#";
     }
-    public abstract String getTitle();
-    public abstract void setTitle();
-    public abstract float getPrice();
-    public abstract void setPrice();
-    public abstract short getRate();
-    public abstract void setRate();
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){ // if the user has access as the admin
+        this.title = title;
+    }
+    public float getPrice(){
+        return this.price;
+    }
+    public void setPrice(float price){ // if the user has access as the admin
+        this.price = price;
+    }
+    public int getRate(){
+        return this.getRate();
+    }
+    public void updateRate(int rate){ // users can rate each product
+        this.rate = (this.rate * ratersNum) + rate / ++ratersNum;
+    }
 
 }
