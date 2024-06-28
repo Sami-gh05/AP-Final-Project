8f6ddeb232637d87a488a051e7e00a4bb6b057e7
Payam Divsalar <payamdivsalar@aut.ac.ir>
2024-06-28 21:51:34 +0330

    Update UserProfilePanel.java

--- store/src/graphics/UserProfilePanel.java
+++ store/src/graphics/UserProfilePanel.java
@@ -1,4 +1,36 @@
 package graphics;
 
+import account.User;
+
+import javax.swing.*;
+
 public class UserProfilePanel {
+
+    private JPanel panel;
+    private JButton exitButton;
+    private UserMainPanel userMainPanel;
+    private User user;
+
+    public UserProfilePanel(UserMainPanel userMainPanel, User user) {
+        panel = new JPanel();
+        this.user = user;
+        this.userMainPanel = userMainPanel;
+        /*
+        exitButton = new JButton("Exit");
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userMainPanel.showUserMainPanel();
+            }
+        });
+
+         */
+
+
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
 }
