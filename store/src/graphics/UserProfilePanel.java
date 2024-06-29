ed8835ef97f80259dad938f91ffc4611b3c83134
PayamDivsalar <payamdivsalar@aut.ac.ir>
2024-06-29 07:19:11 +0330

    show profile panel configured.

--- store/src/graphics/UserProfilePanel.java
+++ store/src/graphics/UserProfilePanel.java
@@ -1,36 +1,76 @@
 package graphics;
 
 import account.User;
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class UserProfilePanel {
-
-    private JPanel panel;
-    private JButton exitButton;
-    private UserMainPanel userMainPanel;
+public class UserProfilePanel extends JPanel {
     private User user;
 
-    public UserProfilePanel(UserMainPanel userMainPanel, User user) {
-        panel = new JPanel();
+    private JLabel nameLabel;
+    private JLabel usernameLabel;
+    private JLabel addressLabel;
+    private JLabel phoneNumberLabel;
+
+    private JTextField nameField;
+    private JTextField usernameField;
+    private JTextField addressField;
+    private JTextField phoneNumberField;
+
+    private JButton editButton;
+    private JButton increaseBalanceButton;
+    private JButton exitButton;
+
+    public UserProfilePanel(User user) {
         this.user = user;
-        this.userMainPanel = userMainPanel;
-        /*
+
+        setLayout(new GridLayout(5, 2));
+
+        nameLabel = new JLabel("Name:");
+        usernameLabel = new JLabel("Username:");
+        addressLabel = new JLabel("Address:");
+        phoneNumberLabel = new JLabel("Phone Number:");
+
+        nameField = new JTextField(user.getName());
+        usernameField = new JTextField(user.getUserName());
+        addressField = new JTextField(user.getAddress());
+        phoneNumberField = new JTextField(user.getPhoneNumber());
+
+        editButton = new JButton("Edit Profile");
+        increaseBalanceButton = new JButton("Increase Balance");
         exitButton = new JButton("Exit");
 
-        exitButton.addActionListener(new ActionListener() {
+        // Add components to the panel
+        add(nameLabel);
+        add(nameField);
+        add(usernameLabel);
+        add(usernameField);
+        add(addressLabel);
+        add(addressField);
+        add(phoneNumberLabel);
+        add(phoneNumberField);
+        add(editButton);
+        add(increaseBalanceButton);
+
+        editButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                userMainPanel.showUserMainPanel();
+
+                user.setName(nameField.getText());
+                user.setAddress(addressField.getText());
+                user.setPhoneNumber(phoneNumberField.getText());
+
             }
         });
 
-         */
 
 
-    }
 
-    public JPanel getPanel() {
-        return panel;
     }
 }
+
+
+
+
