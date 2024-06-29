fa4a1237686a33ea647e015558262f6106cedb95
Sami-gh05 <sam.ghorbani8490@gmail.com>
2024-06-29 20:00:46 +0330

    class created to show the main panel for both user and admin

--- store/src/graphics/AdminMainPanel.java
+++ store/src/graphics/AdminMainPanel.java
@@ -1,2 +1,26 @@
-package graphics;public class AdminMainPanel {
+package graphics;
+
+import javax.swing.*;
+
+public class AdminMainPanel extends ProductsPanel{
+    @Override
+    public JPanel createHeader(){
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+
+        JButton editProductButton = new JButton("Edit Products");
+        editProductButton.addActionListener(e -> {
+            //TODO
+        });
+
+        JButton userLoistButton = new JButton("Users List");
+        userLoistButton.addActionListener(e -> {
+            super.root.removeAll();
+            super.root.add(new UserListPanel().getMainPanel());
+        });
+
+        header.add(editProductButton);
+        header.add(userLoistButton);
+        return header;
+    }
 }
--- store/src/graphics/UserMainPanel.java
+++ store/src/graphics/UserMainPanel.java
@@ -1,2 +1,32 @@
-package graphics;public class UserMainPanel {
+package graphics;
+
+import account.User;
+import javax.swing.*;
+
+public class UserMainPanel extends ProductsPanel{
+    private User user;
+    public UserMainPanel(User user){
+        this.user = user;
+    }
+    @Override
+    public JPanel createHeader(){
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+
+        JButton userPanelButton = new JButton("User Profile");
+        userPanelButton.addActionListener(e -> {
+            super.root.removeAll();
+            super.root.add(new UserProfilePanel(user));
+        });
+
+        JButton shoppingCard = new JButton("Shopping card");
+        shoppingCard.addActionListener(e -> {
+            // TODO: Implement the logic to show the shopping card panel
+        });
+
+        header.add(userPanelButton);
+        header.add(shoppingCard);
+        return header;
+    }
+
 }
