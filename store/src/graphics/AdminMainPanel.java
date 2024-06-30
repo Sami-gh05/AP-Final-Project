f792c59cf34bb8909b6cbfe0c3f94dcdf6baf1f1
Payam Divsalar <payamdivsalar@aut.ac.ir>
2024-06-30 14:41:58 +0330

    Update AdminMainPanel.java

--- store/src/graphics/AdminMainPanel.java
+++ store/src/graphics/AdminMainPanel.java
@@ -8,15 +8,28 @@ import product.Product;
 import shop.Data;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
-public class AdminMainPanel extends ProductsPanel{
-    private List<Product> products;
-    public AdminMainPanel(List<Product> products){
+public class AdminMainPanel extends ProductsPanel {
+
+    public AdminMainPanel(List<Product> products) {
         super(products);
+
+
+        // Adding header to the north
+        JPanel header = createHeader();
+        root.add(header, BorderLayout.NORTH);
+
+        // Adding product grid to the center
+        JPanel productGrid = showProducts();
+        root.add(productGrid, BorderLayout.CENTER);
+
+        frame.add(root);
     }
+
     @Override
-    public JPanel createHeader(){
+    public JPanel createHeader() {
         JPanel header = new JPanel();
         header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
 
@@ -51,19 +64,23 @@ public class AdminMainPanel extends ProductsPanel{
         header.add(refreshButton);
         return header;
     }
-    //The following method implementations give the access to see, edit or remove products(both cloth and phone) to admin
+
+    // The following method implementations give the access to see, edit, or remove products (both cloth and phone) to the admin
+    @Override
     public JPanel createProductBox(Cloth cloth) {
         JPanel box = new JPanel();
         box.setLayout(new BoxLayout(box, BoxLayout.Y_AXIS));
 
         JLabel productName = new JLabel(cloth.getTitle());
         JLabel productPrice = new JLabel(String.valueOf(cloth.getPrice()));
-        //getting the rate as stars:
+        // Getting the rate as stars:
         String stars = "";
-        for(int i = 0; i < cloth.getRate(); i++)
+        for (int i = 0; i < cloth.getRate(); i++)
             stars += "★";
         JLabel productRating = new JLabel(stars);
-        JLabel image = cloth.getImageLabel();
+        JLabel imageLabel = cloth.getImageLabel() != null ? cloth.getImageLabel() : new JLabel("No Image Available");
+        JButton changeImageButton = new JButton("Change Image"); // Button to change image
+
         JLabel category = new JLabel(cloth.getLabel());
         JLabel size = new JLabel(cloth.getSize());
         JLabel color = new JLabel(cloth.getColor());
@@ -72,7 +89,6 @@ public class AdminMainPanel extends ProductsPanel{
 
         JTextField productNameField = new JTextField();
         JTextField productPriceField = new JTextField();
-        //TODO implement image edit
         JTextField sizeField = new JTextField();
         JTextField colorField = new JTextField();
         JTextField sexField = new JTextField();
@@ -83,24 +99,52 @@ public class AdminMainPanel extends ProductsPanel{
             JOptionPane.showMessageDialog(removeProductButton, "Done");
         });
 
-        JButton editProductButton = new JButton("Confirm edit");
+        JButton editProductButton = new JButton("Confirm Edit");
         editProductButton.addActionListener(e -> {
-            cloth.setTitle(productNameField.getText());
-            productNameField.setText("");
-            cloth.setPrice(Float.parseFloat(productPriceField.getText()));
-            productPriceField.setText("");
-            cloth.setColor(colorField.getText());
-            colorField.setText("");
-            cloth.setSize(sizeField.getText());
-            sizeField.setText("");
-            cloth.setSex(sexField.getText());
-            sexField.setText("");
-            JOptionPane.showMessageDialog(editProductButton, "Done");
+            try {
+                if (!productNameField.getText().isEmpty()) {
+                    cloth.setTitle(productNameField.getText());
+                    productNameField.setText("");
+                }
+                if (!productPriceField.getText().isEmpty()) {
+                    cloth.setPrice(Float.parseFloat(productPriceField.getText()));
+                    productPriceField.setText("");
+                }
+                if (!colorField.getText().isEmpty()) {
+                    cloth.setColor(colorField.getText());
+                    colorField.setText("");
+                }
+                if (!sizeField.getText().isEmpty()) {
+                    cloth.setSize(sizeField.getText());
+                    sizeField.setText("");
+                }
+                if (!sexField.getText().isEmpty()) {
+                    cloth.setSex(sexField.getText());
+                    sexField.setText("");
+                }
+                JOptionPane.showMessageDialog(editProductButton, "Done");
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(editProductButton, "Invalid price format");
+            }
+        });
+
+        // Action listener for "Change Image" button
+        changeImageButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int returnValue = fileChooser.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                // Get the selected file
+                java.io.File selectedFile = fileChooser.getSelectedFile();
+                // Assuming selectedFile is an image file
+                ImageIcon newImageIcon = new ImageIcon(selectedFile.getPath());
+                imageLabel.setIcon(newImageIcon);
+                cloth.setImageIcon(newImageIcon);
+                cloth.setImageLabel(imageLabel); // Update cloth's image label
+            }
         });
 
 
-
-        box.add(image);
+        box.add(changeImageButton);
         box.add(productName);
         box.add(productNameField);
         box.add(productPrice);
@@ -119,18 +163,22 @@ public class AdminMainPanel extends ProductsPanel{
 
         return box;
     }
+
+    @Override
     public JPanel createProductBox(Phone phone) {
         JPanel box = new JPanel();
         box.setLayout(new BoxLayout(box, BoxLayout.Y_AXIS));
 
         JLabel productName = new JLabel(phone.getTitle());
         JLabel productPrice = new JLabel(String.valueOf(phone.getPrice()));
-        //getting the rate as stars:
+        // Getting the rate as stars:
         String stars = "";
-        for(int i = 0; i < phone.getRate(); i++)
+        for (int i = 0; i < phone.getRate(); i++)
             stars += "★";
         JLabel productRating = new JLabel(stars);
-        JLabel image = phone.getImageLabel();
+        JLabel imageLabel = phone.getImageLabel() != null ? phone.getImageLabel() : new JLabel("No Image Available");
+        JButton changeImageButton = new JButton("Change Image");
+
         JLabel category = new JLabel(phone.getLabel());
         JLabel companyName = new JLabel(phone.getCompanyName());
         JLabel color = new JLabel(phone.getColor());
@@ -139,7 +187,6 @@ public class AdminMainPanel extends ProductsPanel{
 
         JTextField productNameField = new JTextField();
         JTextField productPriceField = new JTextField();
-        //TODO implement image edit
         JTextField companyNameField = new JTextField();
         JTextField colorField = new JTextField();
         JTextField modelField = new JTextField();
@@ -150,24 +197,51 @@ public class AdminMainPanel extends ProductsPanel{
             JOptionPane.showMessageDialog(removeProductButton, "Done");
         });
 
-        JButton editProductButton = new JButton("Confirm edit");
+        JButton editProductButton = new JButton("Confirm Edit");
         editProductButton.addActionListener(e -> {
-            phone.setTitle(productNameField.getText());
-            productNameField.setText("");
-            phone.setPrice(Float.parseFloat(productPriceField.getText()));
-            productPriceField.setText("");
-            phone.setColor(colorField.getText());
-            colorField.setText("");
-            phone.setCompanyName(companyNameField.getText());
-            companyNameField.setText("");
-            phone.setModel(modelField.getText());
-            modelField.setText("");
-            JOptionPane.showMessageDialog(editProductButton, "Done");
+            try {
+                if (!productNameField.getText().isEmpty()) {
+                    phone.setTitle(productNameField.getText());
+                    productNameField.setText("");
+                }
+                if (!productPriceField.getText().isEmpty()) {
+                    phone.setPrice(Float.parseFloat(productPriceField.getText()));
+                    productPriceField.setText("");
+                }
+                if (!colorField.getText().isEmpty()) {
+                    phone.setColor(colorField.getText());
+                    colorField.setText("");
+                }
+                if (!companyNameField.getText().isEmpty()) {
+                    phone.setCompanyName(companyNameField.getText());
+                    companyNameField.setText("");
+                }
+                if (!modelField.getText().isEmpty()) {
+                    phone.setModel(modelField.getText());
+                    modelField.setText("");
+                }
+                JOptionPane.showMessageDialog(editProductButton, "Done");
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(editProductButton, "Invalid price format");
+            }
         });
 
+        // Action listener for "Change Image" button
+        changeImageButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int returnValue = fileChooser.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                // Get the selected file
+                java.io.File selectedFile = fileChooser.getSelectedFile();
+                // Assuming selectedFile is an image file
+                ImageIcon newImageIcon = new ImageIcon(selectedFile.getPath());
+                imageLabel.setIcon(newImageIcon);
+                phone.setImageIcon(newImageIcon);
+                phone.setImageLabel(imageLabel);
+            }
+        });
 
-
-        box.add(image);
+        box.add(changeImageButton);
         box.add(productName);
         box.add(productNameField);
         box.add(productPrice);
@@ -185,5 +259,32 @@ public class AdminMainPanel extends ProductsPanel{
         box.add(editProductButton);
 
         return box;
+    }
+
+
+
+    public JPanel showProducts() {
+        JPanel allProductsPanel = new JPanel();
+        allProductsPanel.setLayout(new GridLayout(0, 3, 10, 10)); // 3 columns
+
+        if (products != null) {
+            for (Product product : Data.getProducts()) {
+                if (product instanceof Cloth) {
+                    Cloth cloth = (Cloth) product;
+                    allProductsPanel.add(createProductBox(cloth));
+                } else if (product instanceof Phone) {
+                    Phone phone = (Phone) product;
+                    allProductsPanel.add(createProductBox(phone));
+                }
+            }
+        }
+
+        JScrollPane scrollPane = new JScrollPane(allProductsPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(scrollPane, BorderLayout.CENTER);
+        return panel;
     }
 }
